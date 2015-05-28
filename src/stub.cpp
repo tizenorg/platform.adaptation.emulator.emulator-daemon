41c13b20be2cbe474917d3da6b3c4a7772d52092
Jinhyung Choi <jinhyung2.choi@samsung.com>
2015-05-28 12:07:51 +0900

    profile: added stub for no profile available
    Change-Id: I0f7eb191ec602dffe70d9d27420e482316d85a82
    Signed-off-by: Jinhyung Choi <jinhyung2.choi@samsung.com>

---
+++ src/stub.cpp
@@ -0,0 +1,37 @@
+/*
+ * emulator-daemon
+ *
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact:
+ * Jinhyung Choi <jinhyung2.choi@samsnung.com>
+ * Sangho Park <sangho1206.park@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ * - S-Core Co., Ltd
+ *
+ */
+
+#include "emuld.h"
+
+bool extra_evdi_command(ijcommand* ijcmd)
+{
+    return true;
+}
+
+void add_vconf_map_profile(void)
+{
+}
+
